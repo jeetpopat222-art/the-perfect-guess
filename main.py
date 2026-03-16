11d64fd2eb213aadd1542218a404fb885c17bee3
jeetpopat222-art <jeetpopat222@gmail.com>
2026-03-16 18:03:06 +0530

    number guessing game with play again feature

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+
 import random
 
 attampt = 10
 a = random.randint(1,100)
-
+print(a)
 
 while True:
     user = int(input("enter a number to guess: "))
@@ -18,8 +19,24 @@ while True:
 
     elif user==a:
         print("you win")
-        
+        try_again = input("type YES to play again,else type NO: ").lower()
+        if try_again == "yes".lower():
+            a = random.randint(1,100)
+            attampt = 10
+            print(a)
+            continue
+        else:
+            break
+
     if attampt == 0:
         print("game over")
         print(f"the actual number was {a}")
+        try_again = input("type YES to play again,else type NO: ").lower()
+        if try_again == "yes".lower():
+            a = random.randint(1,100)
+            attampt = 10
+            print(a)
+            continue
+        else:
+            break
         
